d24d78810f818296873ec38df06362d2b8f9f5ef
petra popovic <petrapopovic@ssklivno.net>
2023-04-27 12:40:21 +0200

    napravili smo modele

--- galerija/models.py
+++ galerija/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=70)
+    banner = models.ImageField()
+
+    def __str__(self):
+        return self.title
+    
+class Images(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, null=True, blank=True)    
